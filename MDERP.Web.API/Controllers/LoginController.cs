3c08a2b08178c951a2ffa6a56931417b84f51e2d
齐燕冬 <mlzyqyd@163.com>
2023-02-25 19:40:02 +0800

    Create MDERP Web API project.

---
+++ MDERP.Web.API/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDERP.Web.API.Controllers
+{
+    [ApiController]
+    [Route("user/login")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public JsonResult Index()
+        {
+            return new JsonResult("");
+        }
+    }
+}
---
+++ MDERP.Web.API/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.OpenApi.Models;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#region NLog
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables().Build();
+LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+var logger = NLog.Web.NLogBuilder.ConfigureNLog(LogManager.Configuration).GetCurrentClassLogger();
+builder.Host.UseNLog();
+#endregion
+builder.Services.AddControllers();
+#region Swagger
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MDERP WEB API", Version = "v1" });
+
+    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "bearerAuth"
+        }
+        },
+        new string[] { }
+    }
+    });
+});
+#endregion
+
+
+var app = builder.Build();
+#region cors
+app.UseCors(x => x.SetIsOriginAllowed(_ => true)
+.AllowAnyMethod()
+.AllowAnyHeader()
+.AllowCredentials()
+);
+#endregion
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
